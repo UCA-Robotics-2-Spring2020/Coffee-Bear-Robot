888145b9043d1e3c50e00c0cb8d656d99906e2aa
drfred04 <carlf@uca.edu>
2020-04-09 10:17:13 -0500

    removed some extra text

--- test.py
+++ test.py
@@ -4,15 +4,11 @@
 drfred = "DrFred"
 print(drfred)
 
-<<<<<<< HEAD
 tf = "test change"
 print(tf)
-=======
-<<<<<<< HEAD
+
 matpat = "Matthew Patton"
 print(matpat)
-=======
+
 drfred_2 = "carlf04"
 print(drfred_2)
->>>>>>> 7d47e40e5be1bf5b26b67bed00614f6a5b7a6c44
->>>>>>> f96043b0f78d37b8a04d23d0b4cc71e2df7916c9
